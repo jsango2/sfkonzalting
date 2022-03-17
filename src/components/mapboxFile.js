125df4a877dc6614ee912d9c71bcb615b1ea414b
jsango2 <jure.sango@gmail.com>
2022-03-17 06:16:05 +0100

    mapbox style edit

--- src/components/mapboxFile.js
+++ src/components/mapboxFile.js
@@ -2,6 +2,7 @@ import React, { Component } from "react"
 import mapboxgl from "mapbox-gl"
 import "./mapstyles.css"
 mapboxgl.accessToken = process.env.GATSBY_MAPBOX_KEY
+
 //test
 class MapboxFile extends Component {
   constructor(props) {
@@ -20,29 +21,6 @@ class MapboxFile extends Component {
       zoom: this.state.zoom,
       attributionControl: false,
     })
-    // const geojson = {
-    //   type: "FeatureCollection",
-    //   features: [
-    //     {
-    //       type: "Feature",
-    //       geometry: {
-    //         type: "Point",
-    //         coordinates: [15.234627454886867, 44.1248],
-    //       },
-    //       properties: {
-    //         title: "Marker 2",
-    //         description: "San Francisco, California",
-    //       },
-    //     },
-    //   ],
-    // }
-    // geojson.features.forEach(function (marker) {
-    //   // create a HTML element for each feature
-    //   const el = document.createElement("div")
-    //   el.className = "marker"
-    //   // make a marker for each feature and add to the map
-    //   new mapboxgl.Marker(el).setLngLat(marker.geometry.coordinates).addTo(map)
-    // })
   }
 
   render() {
